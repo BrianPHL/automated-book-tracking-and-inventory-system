82e9843f5256348151f1fcd37ce7d3210c839470
Brian <brianpasco1206@gmail.com>
2023-04-16 14:39:25 +0800

    refactor(express): initialized mysql2 and routes

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,23 @@
-export {};
+// ? Dependencies
+import express from 'express';
+import { createPool } from 'mysql2';
+import * as dotenv from 'dotenv';
+// ? Routes
+import loginRoute from './routes/login.js';
+import errorRoute from './routes/error.js';
+import staffDashboardRoute from './routes/staff-dashboard.js';
+import studentDashboardRoute from './routes/student-dashboard.js';
+dotenv.config();
+const app = express();
+const pool = createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME
+});
+app.use('/static', express.static('public'));
+app.get('/', loginRoute);
+app.get('/error', errorRoute);
+app.get('/staff-dashboard', staffDashboardRoute);
+app.get('/student-dashboard', studentDashboardRoute);
+app.listen(process.env.EXPRESS_PORT);
