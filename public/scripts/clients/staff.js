b74daf179510bf4419e968ccc08d77a71ffa4e0d
Brian <brianpasco1206@gmail.com>
2023-05-07 18:49:42 +0800

    feat(staff/scripts): add elements selector

--- public/scripts/clients/staff.js
+++ public/scripts/clients/staff.js
@@ -1,4 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const staff = {
+        overview: {
+            availability: document.querySelector('#ov-av-data-count'),
+            borrowed: document.querySelector('#ov-br-data-count'),
+            due: document.querySelector('#ov-du-data-count'),
+            registered: document.querySelector('#ov-rs-data-count')
+        },
+        notifications: {
+            container: document.querySelector('#nf-data-entries'),
+            data: {
+                message: document.querySelector('#nf-data-message'),
+                date: document.querySelector('#nf-data-date')
+            }
+        },
+        availability: {
+            container: document.querySelector('#av-data-entries'),
+            data: {
+                title: document.querySelector('#av-data-title'),
+                author: document.querySelector('#av-data-author'),
+                category: document.querySelector('#av-data-category'),
+                date: document.querySelector('#av-data-date')
+            },
+            actions: {
+                lend: document.querySelector('#av-actions-lend'),
+                edit: document.querySelector('#av-actions-edit')
+            }
+        },
+        borrowed: {
+            container: document.querySelector('#br-data-entries'),
+            header: {
+                title: document.querySelector('#br-header-title'),
+                viewDetails: document.querySelector('#br-header-viewDetails')
+            },
+            data: {
+                borrower: document.querySelector('#br-data-borrower'),
+                due: document.querySelector('#br-data-due')
+            },
+            actions: {
+                markAsReturned: document.querySelector('#br-actions-markAsReturned')
+            }
+        }
+    };
     const navigation = document.querySelectorAll('.sidebar-links-nav > a,' +
         '.sidebar-links-librarian > a,' +
         '.sidebar-links-admin > a');
--- src/scripts/typings.d.ts
+++ src/scripts/typings.d.ts
@@ -1,16 +1,94 @@
 interface loginForm {
+
     button: HTMLButtonElement
+
     username: {
+
         label: HTMLLabelElement
         input: HTMLInputElement
+
     }
+
     password: {
+
         label: HTMLLabelElement
         input: HTMLInputElement
+
     }
+
     error: {
+
         container: HTMLDivElement,
         text: HTMLParagraphElement
-    },
+
+    }
+
     container: HTMLFormElement
+
+}
+
+interface dashboard {
+    
+    overview: {
+
+        availability: HTMLElement
+        borrowed: HTMLElement
+        due: HTMLElement
+        registered: HTMLElement
+
+    }
+
+    notifications: {
+
+        container: HTMLDivElement
+
+        data: {
+
+            message: HTMLElement
+            date: HTMLTimeElement
+
+        }
+
+    }
+
+    availability: {
+
+        container: HTMLDivElement
+
+        data: {
+
+            title: HTMLHeadingElement
+            author: HTMLElement
+            category: HTMLElement
+            date: HTMLTimeElement
+
+        }
+
+        actions: {
+            lend: HTMLButtonElement
+            edit: HTMLButtonElement
+        }
+        
+    }
+
+    borrowed: {
+
+        container: HTMLDivElement
+
+        header: {
+
+            title: HTMLHeadingElement
+            viewDetails: HTMLButtonElement
+
+        }
+
+        data: {
+
+            borrower: HTMLElement
+            due: number
+
+        }
+
+    }
+
 }
