5a485da22ee8d310205c68175bc895d9f9785d61
Brian <brianpasco1206@gmail.com>
2023-05-09 19:26:16 +0800

    fix(scripts): dark mode conflict with other pages

--- src/scripts/clients/initialize.ts
+++ src/scripts/clients/initialize.ts
@@ -3,9 +3,14 @@ export const setLightTheme = () => {
     const htmlElement = document.querySelector('html') as HTMLElement;
     const themeBtn = document.querySelector('#hd-actions-theme');
 
-    themeBtn.classList.add('fa-moon')
-    themeBtn.classList.remove('fa-sun')
-    htmlElement.setAttribute('data-theme', 'light')
+    if (htmlElement.getAttribute('data-site') == 'staff') {
+
+        themeBtn.classList.add('fa-moon')
+        themeBtn.classList.remove('fa-sun')
+
+    }
+
+    htmlElement.setAttribute('data-theme', 'light')  
     setPreferredTheme('light')
 }
 export const setDarkTheme = () => {
@@ -13,10 +18,14 @@ export const setDarkTheme = () => {
     const htmlElement = document.querySelector('html') as HTMLElement;
     const themeBtn = document.querySelector('#hd-actions-theme');
 
-    themeBtn.classList.add('fa-sun')
-    themeBtn.classList.remove('fa-moon')
-    htmlElement.setAttribute('data-theme', 'dark')
-    
+    if (htmlElement.getAttribute('data-site') == 'staff') {
+
+        themeBtn.classList.add('fa-sun')
+        themeBtn.classList.remove('fa-moon') 
+
+    }
+
+    htmlElement.setAttribute('data-theme', 'dark')   
     setPreferredTheme('dark')
     
 }
