5dce1ff13f81cd6fd66bd28c2dfcb8380eb9d551
Brian <brianpasco1206@gmail.com>
2023-07-04 00:46:53 +0800

    feat(scripts): add execute database query function

--- public/scripts/utils/server.utils.js
+++ public/scripts/utils/server.utils.js
@@ -1,1 +1,11 @@
-export {};
+import { pool } from "../app.js";
+export const executeDatabaseQuery = async (query, argument, callback) => {
+    pool.query(query, argument, (error, results) => {
+        if (callback) {
+            if (error) {
+                callback(error);
+            }
+            callback(results);
+        }
+    });
+};
---
+++ src/scripts/utils/server.utils.ts
@@ -0,0 +1,24 @@
+import { pool } from "../app.js"
+import { QueryError, RowDataPacket, OkPacket, ResultSetHeader } from "mysql2";
+
+declare type callbackType = QueryError | RowDataPacket[] | RowDataPacket[][] | OkPacket | OkPacket[] | ResultSetHeader
+
+export const executeDatabaseQuery = async (query: string, argument?: string[] | null, callback?: (result: callbackType) => void) => {
+
+    pool.query(query, argument, (error, results) => {
+
+        if (callback) {
+
+            if (error) {
+
+                callback(error)
+    
+            }
+    
+            callback(results)    
+
+        }
+        
+    })
+
+}
