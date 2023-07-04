ee05fd6ca3e67b372de423cec1d58f89be977570
Brian <brianpasco1206@gmail.com>
2023-07-04 12:41:54 +0800

    feat(scripts/routes): add personnel login auth route

--- public/scripts/routes/personnel/login.route.js
+++ public/scripts/routes/personnel/login.route.js
@@ -1,6 +1,8 @@
 import express from "express";
+import { personnelLoginAuth } from "../../controllers/auth.controller.js";
 const personnelLoginRoute = express.Router();
 personnelLoginRoute.get("/", (req, res) => {
     res.sendFile("login.html", { root: "public/views/personnel" });
 });
+personnelLoginRoute.post("/auth", personnelLoginAuth);
 export default personnelLoginRoute;
--- src/scripts/routes/personnel/login.route.ts
+++ src/scripts/routes/personnel/login.route.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { Request, Response } from "express";
+import {personnelLoginAuth } from "../../controllers/auth.controller.js";
 
 const personnelLoginRoute = express.Router();
 
@@ -8,5 +9,7 @@ personnelLoginRoute.get("/", (req: Request, res: Response): void => {
     res.sendFile("login.html", { root: "public/views/personnel" })
 
 })
+
+personnelLoginRoute.post("/auth", personnelLoginAuth)
 
 export default personnelLoginRoute
