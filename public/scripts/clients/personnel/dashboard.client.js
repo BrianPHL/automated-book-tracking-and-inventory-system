508304fbb3f34267276c80fa13646234fa260533
Brian <brianpasco1206@gmail.com>
2023-08-01 22:16:29 +0800

    refactor(scripts: clients): beautify code

--- public/scripts/clients/personnel/dashboard.client.js
+++ public/scripts/clients/personnel/dashboard.client.js
@@ -1,15 +1,7 @@
 import * as utils from "../../utils/client.utils.js";
 document.addEventListener('DOMContentLoaded', () => {
-    const htmlElement = document.querySelector('html');
-    const bodyElement = htmlElement.querySelector('body');
-    const header = bodyElement.querySelector('header');
-    const nav = header.querySelector('nav');
-    const navTabs = nav.querySelector('.tabs');
-    const navTabsBtn = navTabs.querySelectorAll('div');
-    const navActions = nav.querySelector('.actions');
-    const navActionsTheme = navActions.querySelector('.themeSwitch');
-    const navActionsLogout = navActions.querySelector('.logout');
-    const mainTabs = bodyElement.querySelectorAll('main');
+    const bodyElement = document.querySelector('body');
+    const nav = bodyElement.querySelector('header > nav');
     utils.setPreferredTheme((cbData) => {
         !cbData.savedTheme
             ? (!cbData.preferredTheme
@@ -19,46 +11,79 @@ document.addEventListener('DOMContentLoaded', () => {
                 ? utils.setLightTheme()
                 : utils.setDarkTheme());
     });
-    navActionsTheme.addEventListener('click', (event) => {
-        event.preventDefault();
-        const currentTheme = localStorage.getItem('theme');
-        currentTheme === 'light'
-            ? utils.setDarkTheme()
-            : utils.setLightTheme();
-    });
-    navActionsLogout.addEventListener('click', (event) => {
-        event.preventDefault();
-        navActionsLogout.innerHTML =
-            `
-            <i class="fa-duotone fa-loader fa-spin-pulse"></i>
-            <h2>Logging out...</h2>
-        `;
-        setTimeout(async () => {
-            try {
-                const response = await fetch('/personnel/logout', {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' }
-                });
-                !response.ok
-                    ? window.location.href = `/error?${await response.text()}`
-                    : window.location.href = '/';
-            }
-            catch (err) {
-                const { name, message } = err;
-                window.location.href = `/error?${(await utils.errorPrompt({ title: name, body: message })).toString()}`;
-            }
-        }, 2500);
-    });
-    navTabsBtn.forEach(tabs => {
-        tabs.addEventListener('click', (event) => {
-            const navTab = event.target;
-            const mainTab = bodyElement.querySelector(`main[data-tab="${navTab.classList[0]}"]`);
-            navTabsBtn.forEach(tabs => { tabs.classList.remove('active'); });
-            mainTabs.forEach(tabs => { tabs.style.display = 'none'; });
-            navTab.classList.add('active');
-            mainTab.style.display = 'grid';
-            utils.setDashboardData('personnel', navTab.classList[0]);
+    const navTabs = () => {
+        const navTabs = nav.querySelectorAll('.tabs > div');
+        const mainTabs = bodyElement.querySelectorAll('main');
+        navTabs.forEach(tab => {
+            tab.addEventListener('click', (event) => {
+                const navTab = event.target;
+                const mainTab = bodyElement.querySelector(`main[data-tab="${navTab.classList[0]}"]`);
+                navTabs.forEach(tab => { tab.classList.remove('active'); });
+                mainTabs.forEach(tab => { tab.style.display = 'none'; });
+                navTab.classList.add('active');
+                mainTab.style.display = 'grid';
+                utils.setDashboardData('personnel', navTab.classList[0]);
+            });
         });
-    });
+    };
+    navTabs();
+    const navActions = () => {
+        const navActions = nav.querySelector('.actions');
+        const navRefresh = navActions.querySelector('.refresh');
+        const navthemeSwitch = navActions.querySelector('.themeSwitch');
+        const navLogout = navActions.querySelector('.logout');
+        navRefresh.addEventListener('click', (event) => {
+            event.preventDefault();
+            navRefresh.innerHTML =
+                `
+                <i class="fa-regular fa-redo fa-spin-pulse"></i>
+                <h2>Refreshing...</h2>
+            `;
+            setTimeout(async () => {
+                navRefresh.innerHTML =
+                    `
+                    <i class="fa-regular fa-redo"></i>
+                    <h2>Refresh</h2>
+                `;
+                utils.setDashboardData('personnel');
+            }, 2500);
+        });
+        navthemeSwitch.addEventListener('click', (event) => {
+            event.preventDefault();
+            const currentTheme = localStorage.getItem('theme');
+            currentTheme === 'light'
+                ? utils.setDarkTheme()
+                : utils.setLightTheme();
+        });
+        navLogout.addEventListener('click', (event) => {
+            event.preventDefault();
+            navLogout.innerHTML =
+                `
+                <i class="fa-duotone fa-loader fa-spin-pulse"></i>
+                <h2>Logging out...</h2>
+            `;
+            setTimeout(async () => {
+                try {
+                    const response = await fetch('/personnel/logout', {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' }
+                    });
+                    navLogout.innerHTML =
+                        `
+                        <i class="fa-regular fa-right-from-bracket"></i>
+                        <h2>Logout</h2>
+                    `;
+                    !response.ok
+                        ? window.location.href = `/error?${await response.text()}`
+                        : window.location.href = '/';
+                }
+                catch (err) {
+                    const { name, message } = err;
+                    window.location.href = `/error?${(await utils.errorPrompt({ title: name, body: message })).toString()}`;
+                }
+            }, 2500);
+        });
+    };
+    navActions();
     utils.setDashboardData('personnel');
 });
--- src/scripts/clients/personnel/dashboard.client.ts
+++ src/scripts/clients/personnel/dashboard.client.ts
@@ -2,20 +2,8 @@ import * as utils from "../../utils/client.utils.js"
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const htmlElement: HTMLElement = document.querySelector('html')
-    const bodyElement: HTMLBodyElement = htmlElement.querySelector('body')
-
-    const header: HTMLElement = bodyElement.querySelector('header')
-    const nav: HTMLElement = header.querySelector('nav')
-    
-    const navTabs: HTMLDivElement = nav.querySelector('.tabs')
-    const navTabsBtn: NodeListOf<HTMLDivElement> = navTabs.querySelectorAll('div')
-
-    const navActions: HTMLDivElement = nav.querySelector('.actions')
-    const navActionsTheme: HTMLButtonElement = navActions.querySelector('.themeSwitch')
-    const navActionsLogout: HTMLButtonElement = navActions.querySelector('.logout')
-
-    const mainTabs: NodeListOf<HTMLElement> = bodyElement.querySelectorAll('main')
+    const bodyElement: HTMLBodyElement = document.querySelector('body')
+    const nav: HTMLElement = bodyElement.querySelector('header > nav')
 
     utils.setPreferredTheme((cbData) => {
 
@@ -37,73 +25,121 @@ document.addEventListener('DOMContentLoaded', () => {
         
     })
 
-    navActionsTheme.addEventListener('click', (event) => {
+    const navTabs = () => {
 
-        event.preventDefault()
+        const navTabs: NodeListOf<HTMLDivElement> = nav.querySelectorAll('.tabs > div')
+        const mainTabs: NodeListOf<HTMLElement> = bodyElement.querySelectorAll('main')
 
-        const currentTheme = localStorage.getItem('theme')
+        navTabs.forEach(tab => {
 
-        currentTheme === 'light'
-        ? utils.setDarkTheme()
-        : utils.setLightTheme()
-
-    })
-
-    navActionsLogout.addEventListener('click', (event) => {
-
-        event.preventDefault()
-
-        navActionsLogout.innerHTML =
-        `
-            <i class="fa-duotone fa-loader fa-spin-pulse"></i>
-            <h2>Logging out...</h2>
-        `
-
-        setTimeout(async () => {
-
-            try {
-
-                const response: Response = await fetch('/personnel/logout', {
-        
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' }
+            tab.addEventListener('click', (event: MouseEvent) => {
                 
-                })
+                const navTab = event.target as HTMLElement
+                const mainTab: HTMLDivElement = bodyElement.querySelector(`main[data-tab="${ navTab.classList[0] }"]`)
+                
+                navTabs.forEach(tab => { tab.classList.remove('active') })
+                mainTabs.forEach(tab => { tab.style.display = 'none' })
+                
+                navTab.classList.add('active')
+                mainTab.style.display = 'grid'
+    
+                utils.setDashboardData('personnel', navTab.classList[0])
+    
+            })
+    
+        })
 
-                !response.ok
-                ? window.location.href = `/error?${ await response.text() }`
-                : window.location.href = '/'
+    }
+    navTabs()
 
-            } catch(err) {
-            
-                const { name, message } = err
+    const navActions = () => {
 
-                window.location.href = `/error?${ (await utils.errorPrompt({title: name, body: message})).toString() }`
+        const navActions: HTMLDivElement = nav.querySelector('.actions')
+        const navRefresh: HTMLButtonElement = navActions.querySelector('.refresh')
+        const navthemeSwitch: HTMLButtonElement = navActions.querySelector('.themeSwitch')
+        const navLogout: HTMLButtonElement = navActions.querySelector('.logout')
 
-            }
+        navRefresh.addEventListener('click', (event) => {
 
-        }, 2500)
+            event.preventDefault()
 
-    })
+            navRefresh.innerHTML =
+            `
+                <i class="fa-regular fa-redo fa-spin-pulse"></i>
+                <h2>Refreshing...</h2>
+            `
 
-    navTabsBtn.forEach(tabs => {
+            setTimeout(async () => {
 
-        tabs.addEventListener('click', (event: MouseEvent) => {
-            
-            const navTab = event.target as HTMLElement
-            const mainTab: HTMLDivElement = bodyElement.querySelector(`main[data-tab="${ navTab.classList[0] }"]`)
-            
-            navTabsBtn.forEach(tabs => { tabs.classList.remove('active') })
-            mainTabs.forEach(tabs => { tabs.style.display = 'none' })
-            
-            navTab.classList.add('active')
-            mainTab.style.display = 'grid'
-
-            utils.setDashboardData('personnel', navTab.classList[0])
+                navRefresh.innerHTML =
+                `
+                    <i class="fa-regular fa-redo"></i>
+                    <h2>Refresh</h2>
+                `
+                
+                utils.setDashboardData('personnel')
+                
+            }, 2500)
 
         })
 
-    })
+        navthemeSwitch.addEventListener('click', (event) => {
+
+            event.preventDefault()
+    
+            const currentTheme = localStorage.getItem('theme')
+    
+            currentTheme === 'light'
+            ? utils.setDarkTheme()
+            : utils.setLightTheme()
+    
+        })
+        
+        navLogout.addEventListener('click', (event) => {
+    
+            event.preventDefault()
+    
+            navLogout.innerHTML =
+            `
+                <i class="fa-duotone fa-loader fa-spin-pulse"></i>
+                <h2>Logging out...</h2>
+            `
+    
+            setTimeout(async () => {
+    
+                try {
+    
+                    const response: Response = await fetch('/personnel/logout', {
+            
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' }
+                    
+                    })
+
+                    navLogout.innerHTML =
+                    `
+                        <i class="fa-regular fa-right-from-bracket"></i>
+                        <h2>Logout</h2>
+                    `
+    
+                    !response.ok
+                    ? window.location.href = `/error?${ await response.text() }`
+                    : window.location.href = '/'
+    
+                } catch(err) {
+                
+                    const { name, message } = err
+    
+                    window.location.href = `/error?${ (await utils.errorPrompt({title: name, body: message})).toString() }`
+    
+                }
+    
+            }, 2500)
+    
+        })
+
+    }
+    navActions()
 
     utils.setDashboardData('personnel')
 
