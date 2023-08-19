9596ca9062d211f341d0cabd96e2077098fe8bcb
Brian <brianpasco1206@gmail.com>
2023-08-20 01:15:01 +0800

    feat(scripts: personnel): add table actions route

--- public/scripts/routes/personnel/table.route.js
+++ public/scripts/routes/personnel/table.route.js
@@ -2,4 +2,5 @@ import express from "express";
 import * as controller from "../../controllers/personnel.controller.js";
 const personnelTableRoute = express.Router();
 personnelTableRoute.get("/:table/search/:query", controller.personnelTableSearch);
+personnelTableRoute.post("/:tab/actions/:type", controller.personnelTableActions);
 export default personnelTableRoute;
--- src/scripts/routes/personnel/table.route.ts
+++ src/scripts/routes/personnel/table.route.ts
@@ -4,5 +4,6 @@ import * as controller from "../../controllers/personnel.controller.js";
 const personnelTableRoute = express.Router();
 
 personnelTableRoute.get("/:table/search/:query", controller.personnelTableSearch)
+personnelTableRoute.post("/:tab/actions/:type", controller.personnelTableActions)
 
 export default personnelTableRoute
