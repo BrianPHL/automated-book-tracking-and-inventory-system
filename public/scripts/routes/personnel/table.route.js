85e7369dd3a82b0dde6aecceed425503e7a7d0b8
Brian <brianpasco1206@gmail.com>
2023-08-20 01:15:35 +0800

    refactor(scripts: personnel): improve search route

--- public/scripts/routes/personnel/table.route.js
+++ public/scripts/routes/personnel/table.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import * as controller from "../../controllers/personnel.controller.js";
 const personnelTableRoute = express.Router();
-personnelTableRoute.get("/:table/search/:query", controller.personnelTableSearch);
+personnelTableRoute.get("/:tab/search/:query", controller.personnelTableSearch);
 personnelTableRoute.post("/:tab/actions/:type", controller.personnelTableActions);
 export default personnelTableRoute;
--- src/scripts/routes/personnel/table.route.ts
+++ src/scripts/routes/personnel/table.route.ts
@@ -3,7 +3,7 @@ import * as controller from "../../controllers/personnel.controller.js";
 
 const personnelTableRoute = express.Router();
 
-personnelTableRoute.get("/:table/search/:query", controller.personnelTableSearch)
+personnelTableRoute.get("/:tab/search/:query", controller.personnelTableSearch)
 personnelTableRoute.post("/:tab/actions/:type", controller.personnelTableActions)
 
 export default personnelTableRoute
